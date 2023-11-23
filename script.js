f7d426a77a6c5cc9ba4c2f68abbdd652bac9994b
Vishal-Agarwal77 <108511250+Vishal-Agarwal77@users.noreply.github.com>
2023-11-23 23:05:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const input=document.getElementById("fname");
+input.addEventListener('focusout', ()=>{
+	let str=input.value;
+	str=str.toUpperCase();
+	input.value=str;
+});
